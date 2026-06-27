aee511c0f6961670c6b1eafc9379ee95ad8758fe
Yun-Syuan <syuanwork0912@gmail.com>
2026-06-27 14:34:00 +0800

    modify

---
+++ 0627/lesson2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.title("手機品牌市占率圓餅圖")
+
+labels = ["Nokia", "Samsung", "Apple", "Lumia"]
+sizes = [20, 30, 45, 10]
+colors = ["yellow", "green", "red", "blue"]
+explode = (0.3, 0, 0, 0)
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, colors=colors, explode=explode,
+       shadow=True, autopct="%1.1f%%", startangle=180)
+ax.axis("equal")
+
+st.pyplot(fig)
